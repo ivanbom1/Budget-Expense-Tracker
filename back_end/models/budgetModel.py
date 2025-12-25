d70ac5a4bb0ddc71b95e5e313c4de0caab2314f1
Mohith <mohith.ayanuru@gmail.com>
2025-12-25 20:22:57 +0100

    added budgetModel and database utilities

--- back_end/models/budgetModel.py
+++ back_end/models/budgetModel.py
@@ -1,24 +1,29 @@
 from ..db_connect import Database
 
-class ModelName:
+class Budget:
     table_name = "budgets"
 
     @staticmethod
-    def find_all():
-        ...
-
-    @staticmethod
-    def find_by_id(id):
-        ...
+    def find_by_month(user_id, month, year):
+        return Database.query(f"""SELECT * FROM {Budget.table_name} WHERE id=%s AND month=%s AND year=%s LIMIT 1""", (user_id,month, year),one=True)
+    
 
     @staticmethod
     def create(data):
-        ...
+        sql=f"""INSERT INTO {Budget.table_name}(amount,month, year, user_id)values (%s,%s,%s) RETURNING id"""
+        budget_id=Database.query(sql,(data['amount'],data['month'],data['year'],data['user_id']),
+        one=True)[0]
+
+        return Budget.find_by_month(data['user_id'],data['month'],data['year'])
+    
 
     @staticmethod
-    def update(id, data):
-        ...
+    def update(budget_id, data):
+        sql=f"""UPDATE{Budget.table_name} SET amount=%s, where id=%s """
+        Database.execute(sql,(data['amount'],budget_id))
+        return Database.query(f"SELECT * FROM {Budget.table_name} WHERE id=%s LIMIT 1",(budget_id), one=True)
 
     @staticmethod
-    def delete(id):
-        ...
+    def delete(budget_id):
+        Database.execute(f"DELETE FROM {Budget.table_name} WHERE id=%s",(budget_id))
+        return True
