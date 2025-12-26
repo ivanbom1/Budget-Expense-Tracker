1218e18ec8bc7c6a99e2a473251fbd80fe3cfa2c
ivan.bomko <vanya.bomko@gmail.com>
2025-12-26 12:29:11 +0100

    starter pocket model properties

--- back-end/models/pocketModel.py
+++ back-end/models/pocketModel.py
@@ -1,5 +1,8 @@
 from userModel import User
 
+#from extensions import db
+from app import db
+
 class Pocket(User):
     
     def __init__(self, userId, pocket_name, description=None, balance=0, goal=0, currency=None):
@@ -10,6 +13,9 @@ class Pocket(User):
         self.balance = balance
         self.goal = goal #optional saving goal on the current pocket
         self.currency = currency
+        
             
     def __str__(self):
         return (f"{self.pocket_name} pocket has: {self.balance}{self.currency}/{self.goal}{self.currency}.") if self.goal else (f"{self.pocket_name} pocket has: {self.balance}{self.currency}.")
+    
+    
