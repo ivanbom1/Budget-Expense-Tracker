430b4448c91330e33faba956fd9827cadc24d141
ivan.bomko <vanya.bomko@gmail.com>
2025-12-27 15:35:17 +0100

    initial pocket routes file added

---
+++ back-end/routes/pocketRoutes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from ..controllers.pocketController import PocketController
+
+pocket_bp = Blueprint('pocket', __name__)
+
+pocket_bp.route('/pockets/<int:pocket_id>', methods=['GET'])(PocketController.get_pocket)
+pocket_bp.route('/users/<int:user_id>/pockets', methods=['GET'])(PocketController.get_user_pockets)
+pocket_bp.route('/users/<int:user_id>/pockets', methods=['POST'])(PocketController.create_pocket)
+pocket_bp.route('/pockets/<int:pocket_id>', methods=['PUT'])(PocketController.update_pocket)
+pocket_bp.route('/pockets/<int:pocket_id>', methods=['DELETE'])(PocketController.delete_pocket)
